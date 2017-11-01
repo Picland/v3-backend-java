d5ddbcf0800b16748a10dab42e66c0fd375d3737
muguan <dgq.cs.se@gmail.com>
2017-11-01 09:05:24 +0800

    support spring MVC request forward

--- user/src/main/java/com/earth/user/config/EarthUserConfig.java
+++ user/src/main/java/com/earth/user/config/EarthUserConfig.java
@@ -2,12 +2,11 @@ package com.earth.user.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.earth.user.*.service.impl", "com.earth.user.*.dao.impl"})
 //@Import() can import configs
-@ImportResource({"classpath:applicationContext.xml"})
+@ImportResource({"classpath*:spring/applicationContext.xml"})
 public class EarthUserConfig {
 }
--- user/src/main/java/com/earth/user/config/SpringMVCConfig.java
+++ user/src/main/java/com/earth/user/config/SpringMVCConfig.java
@@ -1,11 +1,10 @@
 package com.earth.user.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan(basePackages = "com.earth.user.controller")
-@ImportResource({"classpath:springMVC-servlet.xml"})
+//@ComponentScan(basePackages = "com.earth.user.controller")
+@ImportResource({"classpath*:spring/springMVC-servlet.xml"})
 public class SpringMVCConfig {
 }
