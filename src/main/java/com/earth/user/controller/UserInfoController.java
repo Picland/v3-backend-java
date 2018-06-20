f78d7708597fb59811d27f430c5bc11e82bc15f1
muguan <dgq.cs.se@gmail.com>
2018-06-20 23:15:37 +0800

    PUT and PATCH return 204, POST return 201 with location, support delete

--- src/main/java/com/earth/user/controller/UserInfoController.java
+++ src/main/java/com/earth/user/controller/UserInfoController.java
@@ -2,7 +2,10 @@ package com.earth.user.controller;
 
 import com.earth.user.bean.UserBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.sql.Timestamp;
 
@@ -11,7 +14,7 @@ import java.sql.Timestamp;
 public class UserInfoController {
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public UserBean getUserById(@PathVariable String id) {
+  public ResponseEntity<?> getUserById(@PathVariable String id) {
     UserBean userBean = new UserBean();
     userBean.setUserId(id);
     userBean.setPhoneNumber("18962000003");
@@ -20,24 +23,28 @@ public class UserInfoController {
     userBean.setEmail("ww@qq.com");
     userBean.setAvatar("upload_584d24786e923bc1a4f384ebf7ea77a5.jpg");
     userBean.setCreatedDateTime(new Timestamp(System.currentTimeMillis()));
-    return userBean;
+    return ResponseEntity.ok().body(userBean);
   }
 
-  @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public UserBean replaceUser(@PathVariable String id, @RequestBody UserBean newUser) {
-    return newUser;
+  @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> replaceUser(@PathVariable String id, @RequestBody UserBean newUser) {
+    return ResponseEntity.noContent().build();
   }
 
-  @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public UserBean mergeUser(@PathVariable String id, @RequestBody UserBean newUser) {
-    return newUser;
+  @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> mergeUser(@PathVariable String id, @RequestBody UserBean newUser) {
+    return ResponseEntity.noContent().build();
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public UserBean createUser(@RequestBody UserBean newUser) {
-    return newUser;
+  public ResponseEntity<?> createUser(@RequestBody UserBean newUser, UriComponentsBuilder uriBuilder) {
+    UriComponents uriComponents = uriBuilder.path("/users/{id}").buildAndExpand(newUser.getUserId());
+    return ResponseEntity.created(uriComponents.toUri()).body(newUser);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<?> deleteUser(@PathVariable String id) {
+    return ResponseEntity.noContent().build();
   }
 
 }
